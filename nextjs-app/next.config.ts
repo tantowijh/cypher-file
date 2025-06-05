a278698ecab259a2480a67c4dd071a093f243f21
Thonthowie Jauharie <tjmailone@gmail.com>
2025-06-05 11:58:13 +0700

    fix

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -1,19 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination:
-  //         process.env.NEXT_PUBLIC_API_URL
-  //           ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-  //           : process.env.NODE_ENV === "development"
-  //             ? "http://127.0.0.1:8000/api/:path*"
-  //             : "http://backend:8000/api/:path*",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NEXT_PUBLIC_API_URL
+            ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+            : process.env.NODE_ENV === "development"
+              ? "http://127.0.0.1:8000/api/:path*"
+              : "http://backend:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
